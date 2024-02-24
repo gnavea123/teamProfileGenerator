3d2c99566a9b37a92c5672a4c691f562ea33c42d
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-02-24 18:57:46 +0000

    Corrected issues with access to lib resources.

--- starter/index.js
+++ starter/index.js
@@ -1,16 +1,23 @@
 const fileName = "team.html";
+// const Manager = require("../lib/Manager.js");
 const Manager = require("./lib/Manager");
+// const Engineer = require("/lib/Engineer.js");
 const Engineer = require("./lib/Engineer");
+
+// const Intern = require("/lib/Intern.js");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 // create the html pages needed
-const util = require("lib");
-const generateHTML = require("./lib/generateHTML.js");
+//const util = require("./lib");
+const util = require("./lib");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const generateHTML = require("./lib/generateHTML.js");
+const generateHTML = require("./lib/generateHTML");
+
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 //const render = require("./src/page-template.js");
 
@@ -50,7 +57,7 @@ const init = async () => {
     console.log("value of userInfo=");
     console.log(userInfo);
     const html = generateHTML(answers, userInfo);
-    console.log("value of markdown");
+    //console.log("value of markdown");
     //console.log(html);
 
     await writeFileAsync(fileName, html);
